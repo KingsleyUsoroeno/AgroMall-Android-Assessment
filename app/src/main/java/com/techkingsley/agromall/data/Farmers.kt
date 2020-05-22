76712b9bd10c95a8167e8f036ae1baa85a6f7d45
KingsleyUsoroeno <kingsleyenou@gmail.com>
2020-05-22 13:00:53 +0100

    reworked @FarmersRecyclerAdapter and implemented UI redesign

--- app/src/main/java/com/techkingsley/agromall/data/Farmers.kt
+++ app/src/main/java/com/techkingsley/agromall/data/Farmers.kt
@@ -1,11 +1,14 @@
 package com.techkingsley.agromall.data
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 import java.util.*
 
+@Parcelize
 @Entity(tableName = "farmers", indices = [Index(value = ["phoneNumber", "email"], unique = true)])
 data class Farmers @JvmOverloads constructor(
     @PrimaryKey @ColumnInfo(name = "farmerID") var id: String = UUID.randomUUID().toString(),
@@ -14,4 +17,4 @@ data class Farmers @JvmOverloads constructor(
     var phoneNumber: String = "",
     var homeAddress: String = "",
     var imageUrl: String = ""
-)
+): Parcelable
--- app/src/main/java/com/techkingsley/agromall/ui/farms/FarmsFragment.kt
+++ app/src/main/java/com/techkingsley/agromall/ui/farms/FarmsFragment.kt
@@ -10,6 +10,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.techkingsley.agromall.R
+import com.techkingsley.agromall.data.Farmers
 import com.techkingsley.agromall.data.Farms
 import com.techkingsley.agromall.databinding.FarmsFragmentBinding
 
@@ -27,19 +28,20 @@ class FarmsFragment : Fragment(R.layout.farms_fragment), FarmsRecyclerAdapter.On
         super.onActivityCreated(savedInstanceState)
         viewBinding = FarmsFragmentBinding.bind(requireView())
         val safeArgs: FarmsFragmentArgs by navArgs()
-        val farmerId = safeArgs.farmerId
-        Log.i(TAG, "farmer id is ${safeArgs.farmerId}")
+        val farmer = safeArgs.farmer
+        Log.i(TAG, "farmer id is ${safeArgs.farmer.id}")
 
-        viewModel.observerFarmsByFarmerID(farmerId, 1).observe(this.viewLifecycleOwner, Observer {
+        viewModel.observerFarmsByFarmerID(farmer.id, 1).observe(this.viewLifecycleOwner, Observer {
             it?.let {
                 Log.i(TAG, "all farms is $it")
-                setUpAdapter(it)
+                setUpAdapter(it, farmer)
             }
         })
     }
 
-    private fun setUpAdapter(farmers: List<Farms>) {
+    private fun setUpAdapter(farmers: List<Farms>, farmer: Farmers) {
         if (farmers.isEmpty()) {
+            viewBinding.textNoFarms.text = "${farmer.fullName} has no registered farms"
             viewBinding.textNoFarms.visibility = View.VISIBLE
             viewBinding.farmsRecyclerView.visibility = View.GONE
         } else {
